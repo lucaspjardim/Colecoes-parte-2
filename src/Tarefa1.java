bc4b1567e17a7bc2a0145193a0f823f7659b28dc
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-09-24 11:38:49 -0300

    Primeira Tarefa1

---
+++ src/Tarefa1.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+/**
+ * Author: Lucas Jardim
+ */
+public class Tarefa1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println(("Digite nomes separados por vírgula: "));
+        String text = s.nextLine();
+
+        String[] namesArray = text.split(",");
+        ArrayList<String> namesList = new ArrayList<String>();
+
+        for (String name : namesArray) {
+            namesList.add(name.trim());
+        }
+
+        Collections.sort(namesList);
+
+        System.out.println("Nomes ordenados: \n"+namesList);
+    }
+}
